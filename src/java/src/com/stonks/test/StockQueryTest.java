0408bef42da4547fd75baac83d6e457ca7954fc7
Chase M. Kinard <kinardc@mail.gvsu.edu>
2021-11-30 15:06:03 -0500

    imported JUnit 5 for testing

--- src/java/src/com/stonks/test/StockQueryTest.java
+++ src/java/src/com/stonks/test/StockQueryTest.java
@@ -9,6 +9,8 @@ import yahoofinance.quotes.stock.StockQuote;
 
 import java.io.IOException;
 
+import org.junit.jupiter.*;
+
 public class StockQueryTest {
     public static void main(String[] args) {
         try {
